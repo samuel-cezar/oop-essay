3fd41a6fd7166bd965d422712d712a072ddce2cd
Samuel Santos <samuxlh@gmail.com>
2024-04-06 01:49:22 -0300

    Desktop programming MVC structure

---
+++ src/main/java/com/samuel/atividadepratica1/Atividadepratica1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.samuel.atividadepratica1;
+
+/**
+ *
+ * @author geniu
+ */
+public class Atividadepratica1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
